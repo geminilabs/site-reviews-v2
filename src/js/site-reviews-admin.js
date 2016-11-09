21fa632b959bd70fd6ce4a6da7738368223b772e
Paul Ryley <paul@geminilabs.io>
2016-11-09 12:15:31 -0500

    include global add-on styles and JS

--- src/js/site-reviews-admin.js
+++ src/js/site-reviews-admin.js
@@ -33,6 +33,13 @@ GLSR.dismissNotices = function()
 	});
 };
 
+GLSR.getURLParameter = function( name )
+{
+	return decodeURIComponent(
+		(new RegExp( '[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)' ).exec( location.search ) || [null, ''])[1].replace( /\+/g, '%20' )
+	) || null;
+};
+
 GLSR.insertNotices = function( notices )
 {
 	notices = notices || false;
@@ -150,6 +157,18 @@ GLSR.pointers = function( pointer )
 	});
 };
 
+GLSR.textareaResize = function( el )
+{
+	var minHeight = 320;
+	var textarea  = el[0];
+
+	textarea.style.height = 'auto';
+
+	textarea.style.height = textarea.scrollHeight > minHeight ?
+		textarea.scrollHeight + 'px' :
+		minHeight + 'px';
+};
+
 GLSR.toggleHiddenField = function( el, bool )
 {
 	var row = x( el ).closest( '.glsr-field' );
@@ -237,6 +256,21 @@ GLSR.pinned.save = function( el )
 
 x( function()
 {
+	var GLSR_fix = GLSR.getURLParameter( 'fix' );
+	var GLSR_textarea = x( '#contentdiv > textarea' );
+
+	if( GLSR_fix ) {
+		x( 'td [data-key="' + GLSR_fix + '"]').focus();
+	}
+
+	if( GLSR_textarea.length ) {
+		GLSR.textareaResize( GLSR_textarea );
+
+		x( document ).on( 'wp-window-resized.editor-expand', function() {
+			GLSR.textareaResize( GLSR_textarea );
+		});
+	}
+
 	x( 'form' ).on( 'change', ':input', GLSR.onFieldChange );
 
 	x( 'form' ).on( 'click', '#clear-log', GLSR.onClearLog );
@@ -247,5 +281,10 @@ x( function()
 
 	x.each( site_reviews_pointers.pointers, function( i, pointer ) {
 		GLSR.pointers( pointer );
+	});
+
+	// WP 4.0 - 4.2 support: toggle list table rows on small screens
+	x( document ).on( 'click', '.branch-4 .toggle-row, .branch-4-1 .toggle-row, .branch-4-2 .toggle-row', function() {
+		x( this ).closest( 'tr' ).toggleClass( 'is-expanded' );
 	});
 });
