f397c644f0efa909c1345bb671e4efbd31622bfe
Paul Ryley <paul@geminilabs.io>
2017-10-17 18:48:34 -0600

    specify abstract release time for addons (v3.0.0)

--- views/menu/addons/addons.php
+++ views/menu/addons/addons.php
@@ -8,14 +8,14 @@
 
 	echo $html->renderPartial( 'addon', [
 		'name'        => 'tripadvisor',
-		'title'       => 'Coming soon...',
+		'title'       => 'Coming soon in v3.0.0',
 		'description' => __( 'Sync your Tripadvisor business reviews with an optional minimum rating and display them on your site.', 'site-reviews' ),
 		'link'        => '',
 	]);
 
 	echo $html->renderPartial( 'addon', [
 		'name'        => 'yelp',
-		'title'       => 'Coming soon...',
+		'title'       => 'Coming soon in v3.0.0',
 		'description' => __( 'Sync your Yelp business reviews with an optional minimum rating and display them on your site.', 'site-reviews' ),
 		'link'        => '',
 	]);
