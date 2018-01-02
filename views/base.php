81463e201662bdc4e812e260951f1a6f58875998
Paul Ryley <paul@geminilabs.io>
2018-01-02 15:49:56 -0700

    Allow automatic view overrides within a theme

--- views/base.php
+++ views/base.php
@@ -1,12 +1,14 @@
 <?php defined( 'WPINC' ) || die;
 
-$file = trailingslashit( __DIR__ ) . "{$view}.php";
-
+$file = sprintf( '%s/site-reviews/%s.php', get_stylesheet_directory(), $view );
+if( !file_exists( $file )) {
+	$file = trailingslashit( __DIR__ )."{$view}.php";
+}
 $file = apply_filters( 'site-reviews/addon/views/file', $file, $view, $data );
 
-if( file_exists( $file ) ) {
+if( file_exists( $file )) {
 	include $file;
 }
 else {
-	$log->error( sprintf( 'File not found: %s', $file ) );
+	$log->error( sprintf( 'File not found: %s', $file ));
 }
