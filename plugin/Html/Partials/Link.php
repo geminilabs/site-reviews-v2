c440279f7efd5c34b1ce78e86f35b750d9f5118d
Paul Ryley <paul@geminilabs.io>
2018-01-08 13:08:12 -0700

    add additional post_id check in Link partial

--- plugin/Html/Partials/Link.php
+++ plugin/Html/Partials/Link.php
@@ -17,14 +17,13 @@ class Link extends Base
 	/**
 	 * Generate a link
 	 *
-	 * @return string
+	 * @return string|null
 	 */
 	public function render()
 	{
 		$args = shortcode_atts( ['post_id' => ''], $this->args );
 		$post = get_post( $args['post_id'] );
-
-		return $args['post_id'] == $post->ID && $post->post_status == 'publish'
+		return isset( $post->ID ) && $post->ID == $args['post_id'] && $post->post_status == 'publish'
 			? sprintf( '<a href="%s">%s</a>', get_the_permalink( $post->ID ), get_the_title( $post->ID ))
 			: '';
 	}
