4b3aafd54957e607f41ca3946b433b2b521157d9
Paul Ryley <paul@geminilabs.io>
2018-07-09 12:11:46 -0600

    Add compatibility for speed-booster-pack plugin

--- compatibility.php
+++ compatibility.php
@@ -10,6 +10,16 @@
 
 defined( 'WPINC' ) || die;
 
+/**
+ * @param array $scriptHandles
+ * @return array
+ * @see https://wordpress.org/plugins/speed-booster-pack/
+ */
+add_filter( 'sbp_exclude_defer_scripts', function( $scriptHandles ) {
+	$scriptHandles[] = 'site-reviews/google-recaptcha';
+	return array_keys( array_flip( $scriptHandles ));
+});
+
 // Wordpress 4.0-4.2 support
 if( !function_exists( 'wp_roles' )) {
 	function wp_roles() {
